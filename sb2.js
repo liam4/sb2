7754f106b36e299635694c23c9075e76d553a9f3
adespotist <jay@backtick.town>
2016-04-14 10:41:03 -0400

    add basic UMD

--- sb2.js
+++ sb2.js
@@ -3,4 +3,25 @@
  * 
  * https://github.com/adespotist/sb2
  */
-
+(function ( root, factory ) {
+	/*
+	 * Make the module available to a load of different module systems, including...
+	 * AMD,
+	 */
+	if ( typeof define === 'function' && define.amd )
+		define( [], factory )
+	
+	/*
+	 * Node and CommonJS, through `module.exports',
+	 */
+	else if ( typeof exports === 'object' )
+		module.exports = factory.call()
+	
+	/*
+	 * and where JavaScript is supposed to be, the browser!
+	 */
+	else
+		root.sb2 = factory.call()
+})(this, function () {
+	return {}
+})
