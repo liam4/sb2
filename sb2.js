a9946eb7b7fafb5b4bcf7123eb976f52a82eb61a
adespotist <jay@backtick.town>
2016-04-15 15:08:47 -0400

    clean up

--- sb2.js
+++ sb2.js
@@ -1,27 +1,20 @@
-/*
- * sb2 by adespotist
- * 
- * https://github.com/adespotist/sb2
- */
 (function ( root, factory ) {
-	/*
-	 * Make the module available to a load of different module systems, including...
-	 * AMD,
-	 */
+	// Make the module available to a load of different module systems, including...
+ 	// AMD,
 	if ( typeof define === 'function' && define.amd )
 		define( [], factory )
-	
-	/*
-	 * Node and CommonJS, through `module.exports',
-	 */
+
+	// Node and CommonJS, through `module.exports',
 	else if ( typeof exports === 'object' )
 		module.exports = factory.call()
-	
-	/*
-	 * and where JavaScript is supposed to be, the browser!
-	 */
+
+	// and where JavaScript is supposed to be, the browser!
 	else
 		root.sb2 = factory.call()
 })(this, function () {
-	return {}
+	// This is where the fun begins.
+	var sb2 = {}
+
+	// Provide the return the object to the module.
+	return sb2
 })
