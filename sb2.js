587b21c745bed323cce2454a3e7443438984c1f3
adespotist <jay@backtick.town>
2016-04-16 11:21:43 -0400

    stages now have parents

--- sb2.js
+++ sb2.js
@@ -52,11 +52,13 @@
 	// that is its parent.
 	sb2.Stage = function Stage ( parent ) {
 		sb2.Scriptable.apply( this, arguments )
-		
+
 		this.penLayerId = 0
 		this.penLayerMD5 = 0
 		this.tempo = 60
 		this.transparency = 0
+
+		this.project.stage = this
 	}
 
 	// Sprites are Scriptables with properties relating to motion such as direction,
