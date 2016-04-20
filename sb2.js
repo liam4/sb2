4b6df3f6944b688063b5acea46d8581913e86e46
adespotist <jay@backtick.town>
2016-04-20 10:48:23 -0400

    fix style

--- sb2.js
+++ sb2.js
@@ -16,7 +16,7 @@
 	var sb2 = {}
 
 	// A Project is simply a collection of Sprites and a Stage.
-	sb2.Project = function Project () {
+	sb2.Project = function Project() {
 		// Create an empty Project with no Sprites and no Stage.
 		this.stage = null
 		this.sprites = []
@@ -24,7 +24,7 @@
 
 	// Actors are things that belong in a Project.
 	// Their only property is a parent Project.
-	sb2.Actor = function Actor ( parent ) {
+	sb2.Actor = function Actor( parent ) {
 		// Actors are, like Scriptables, abstract, so you can't create them.
 		if ( this.constructor === sb2.Actor )
 			throw new Error( 'Cannot instantiate Actor: Abstract' )
@@ -37,7 +37,7 @@
 
 	// Scriptables are things with Scripts.
 	// Everything with Scripts also have, by extension, costumes and sounds.
-	sb2.Scriptable = function Scriptable ( parent ) {
+	sb2.Scriptable = function Scriptable( parent ) {
 		// Scriptables are abstract so you can't create them.
 		if ( this.constructor === sb2.Scriptable )
 			throw new Error( 'Cannot instantiate Scriptable: Abstract' )
@@ -53,7 +53,7 @@
 
 	// Stages are Scriptables that contain some additional information about the Project
 	// that is its parent.
-	sb2.Stage = function Stage ( parent ) {
+	sb2.Stage = function Stage( parent ) {
 		sb2.Scriptable.apply( this, arguments )
 		this.penLayerId = 0
 		this.penLayerMD5 = 0
@@ -64,7 +64,7 @@
 
 	// Sprites are Scriptables with properties relating to motion such as direction,
 	// X position and Y position.
-	sb2.Sprite = function Sprite ( parent, name ) {
+	sb2.Sprite = function Sprite( parent, name ) {
 		sb2.Scriptable.apply( this, [ parent ] )
 		this.name = name
 		this.x = 0
@@ -77,35 +77,34 @@
 	}
 
 	// A Costume is simply an image file with a name.
-	sb2.Costume = function Costume ( file, name ) {
+	sb2.Costume = function Costume( file, name ) {
 		this.name = name
 		this.file = file
 	}
 
 	// A Sound is merely a sound file with a name.
-	sb2.Sound = function Sound ( file, name ) {
+	sb2.Sound = function Sound( file, name ) {
 		this.name = name
 		this.file = file
 	}
 
 	// Blocks are specs with a list of arguments.
 	// Some blocks can also take Scripts as arguments.
-	sb2.Block = function Block ( spec, arguments ) {
-		if ( sb2.Block.specs.map(function (spec) {
+	sb2.Block = function Block( spec, arguments ) {
+		if ( sb2.Block.specs.map(function ( spec ) {
 			return spec[2]
 		}).indexOf( spec ) != -1 ) {
 			this.spec = spec
-			this.arguments = Array.isArray(arguments)
+			this.arguments = Array.isArray( arguments )
 			                 	? arguments
 						: [arguments]
-		} else {
+		} else
 			throw new Error( 'Please provide valid block spec' )
-		}
 	}
 
 	// Scripts are lists of Blocks with coordinates on the scripting area.
 	// Scripts don't need to start with hat blocks - some scripts don't have them.
-	sb2.Script = function Script ( x, y, blocks ) {
+	sb2.Script = function Script( x, y, blocks ) {
 		this.x = x
 		this.y = y
 		this.blocks = blocks
@@ -113,7 +112,7 @@
 
 	// Comments are bits of text attached to the scripting area.
 	// TODO: Attach comments to Blocks
-	sb2.Comment = function Comment ( x, y, message ) {
+	sb2.Comment = function Comment( x, y, message ) {
 		this.x = x
 		this.y = y
 		this.content = message
